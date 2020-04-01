f273b8b6893448d8271112b1ecbe02b304e33e53
DianBing.Shen <shendb0@gmail.com>
2020-04-01 23:28:38 +0800

    20210401  23‘28

---
+++ admin.php
@@ -0,0 +1,10 @@
+<?php
+require("db.php");
+error_reporting(E_ALL ^ E_NOTICE);
+
+function alertMessage($msg, $url)
+{
+    echo "<script>alert('{$msg}');</script>";
+    echo "<script>window.location='{$url}';</script>";
+}
+alertMessage("登录成功！", "userList.html");
--- db.php
+++ db.php
@@ -1,29 +1,46 @@
 <?php
-// function creatCon(){
-//     $conn = mysqli_connect("localhost","root","PU5jYmEn34oeD5dn","sjcbyx",3306);
+function creatCon()
+{
+    $conn = mysqli_connect("localhost", "sjcbyx", "4rfv3edc", "sjcbyx", 3306);
 
-//     #判断是否连接成功
-//     if(!$conn){
-//     echo ("数据库连接失败！". mysqli_connect_error());
-//     }
+    #判断是否连接成功
+    if (!$conn) {
+        echo ("数据库连接失败！" . mysqli_connect_error());
+    }
 
-//     //选择数据库
-//     mysqli_select_db($conn,"sjcbyx");
-//     return $conn;
-// }
+    //选择数据库
+    mysqli_select_db($conn, "sjcbyx");
+    return $conn;
+}
 
+function selectUserInfo($name, $age, $sex)
+{
+    $conn = creatCon();
+    $sql = "select count(*) as num from userinfo where name=" . $name . " and age=" . $age . " and sex=" . $sex;
+    $result = mysqli_query($conn, $sql);
+    $row = mysqli_fetch_assoc($result);
+    $resultNum = $row['num'];
+    if ($resultNum > 0) {
+        mysqli_close($conn);
+        return true;
+    } else {
+        mysqli_close($conn);
+        return false;
+    }
+}
 
-// function select($name,$age,$sex,$userinfo)
-// {
-//     $conn = creatCon();
-//     $sql = "select * from userinfo;"
-// }
-// function update($name,$age,$sex,$userinfo){
-//     $conn = creatCon();
-//     $sql = "insert into userinfo "
-// }
-// function insert($name,$age,$sex,$userinfo){
-//     $conn = creatCon();
-//     $sql = "insert into userinfo "
-// }
-?>
+function updateUserInfo($name, $age, $sex, $userinfo)
+{
+    $conn = creatCon();
+    $sql = "update userinfo set userinfo='" . $userinfo . "' where name=" . $name . " and age=" . $age . " and sex=" . $sex;
+    mysqli_query($conn, $sql);
+    mysqli_close($conn);
+}
+
+function insertUserInfo($name, $age, $sex, $userinfo)
+{
+    $conn = creatCon();
+    $sql = "insert into userinfo (name,age,sex,userinfo) values(" . $name . "," . $age . "," . $sex . "," . $userinfo . "')";
+    mysqli_query($conn, $sql);
+    mysqli_close($conn);
+}
