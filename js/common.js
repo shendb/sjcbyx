30dc0a64e45fe9d4965cb2134e6e4f8106a809fc
DianBing.Shen <shendb0@gmail.com>
2020-04-08 00:30:28 +0800

    2020 04 08 00 30

--- js/common.js
+++ js/common.js
@@ -23,12 +23,12 @@ function imgList() {
                 idStr = "#img" + (i - 1);
                 break;
         }
-        imgStr += "<tr style='border-top:1px solid;'><td>手术前</td><td>手术后一周</td></tr>";
+        imgStr += "<tr><td>手术前</td><td>手术后一周</td></tr>";
         imgStr += "<tr><td><img src='./asset/img/" + i + "/1.jpg' width='100%'></td><td><img src='./asset/img/" + i + "/2.jpg' width='100%'></td></tr>";
-        imgStr += "<tr><td style='text-align:left;' colspan='2'>" + i + "A<input type='range' name='imgRange_" + i + "_WEEK' min='1' max='10' defaultValue='5' onchange='document.getElementById('show_" + i + "_WEEK').innerHTML = value'><span id='show_" + i + "_WEEK'></span></td></tr>";
+        imgStr += "<tr><td style='text-align:left;' colspan='2'>" + i + "A<input type='range' name='imgRange_" + i + "_WEEK' min='1' max='10' defaultValue='5' ></td></tr>";
         imgStr += "<tr><td>手术前</td><td>手术后一个月</td></tr>";
         imgStr += "<tr><td><img src='./asset/img/" + i + "/1.jpg' width='100%'></td><td><img src='./asset/img/" + i + "/3.jpg' width='100%'></td></tr>";
-        imgStr += "<tr id='img" + i + "'><td style='text-align:left;' colspan='2'>" + i + "B<input type='range' name='imgRange_" + i + "_MONTH' min='1' max='10' defaultValue='5' onchange='document.getElementById('show_" + i + "_MONTH').innerHTML = value'><span id='show_" + i + "_MONTH'></span></td></tr>";
+        imgStr += "<tr id='img" + i + "'><td style='text-align:left;' colspan='2'>" + i + "B<input type='range' name='imgRange_" + i + "_MONTH' min='1' max='10' defaultValue='5' ></td></tr>";
         $(idStr).after(imgStr);
     }
 }
