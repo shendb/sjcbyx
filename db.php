dfb3e40b793397501dcf360e60dfddd7a680aa8f
DianBing.Shen <shendb0@gmail.com>
2020-05-18 20:46:01 +0800

    修正 图形居中

--- db.php
+++ db.php
@@ -1,45 +1,38 @@
 <?php
-error_reporting(E_ALL ^ E_NOTICE);
-function creatCon()
-{
-    $conn = mysqli_connect("localhost", "sjcbyx", "4rfv3edc", "sjcbyx", 3306);
+error_reporting ( E_ALL ^ E_NOTICE );
+function creatCon() {
+	$conn = mysqli_connect ( "localhost", "root", "4rfv3edc", "sjcbyx", 3306 );
 
-    #判断是否连接成功
-    if (!$conn) {
-        echo ("数据库连接失败！" . mysqli_connect_error());
-    }
+	// 判断是否连接成功
+	if (! $conn) {
+		echo ("数据库连接失败！" . mysqli_connect_error ());
+	}
 
-    //选择数据库
-    mysqli_select_db($conn, "sjcbyx");
-    return $conn;
+	// 选择数据库
+	mysqli_select_db ( $conn, "sjcbyx" );
+	return $conn;
 }
-
-function selectUserInfo($name)
-{
-    $conn = creatCon();
-    $sql = "select * from userinfo where name=" . $name;
-    $result = mysqli_query($conn, $sql);
-    if (is_resource($result)) {
-        mysqli_close($conn);
-        return true;
-    } else {
-        mysqli_close($conn);
-        return false;
-    }
+function selectUserInfo($name) {
+	$conn = creatCon ();
+	$sql = "select * from userinfo where name='" . $name . "'";
+	$result = mysqli_query ( $conn, $sql );
+	if (is_resource ( $result )) {
+		mysqli_close ( $conn );
+		return true;
+	} else {
+		mysqli_close ( $conn );
+		return false;
+	}
 }
-
-function updateUserInfo($name, $age, $sex, $userinfo)
-{
-    $conn = creatCon();
-    $sql = "update userinfo set userinfo='" . $userinfo . "' where name=" . $name . " and age=" . $age . " and sex=" . $sex;
-    mysqli_query($conn, $sql);
-    mysqli_close($conn);
+function updateUserInfo($name, $age, $sex, $userinfo) {
+	$conn = creatCon ();
+	$sql = "update userinfo set userinfo='" . $userinfo . "' where name='" . $name . "' and age='" . $age . "' and sex='" . $sex . "'";
+	mysqli_query ( $conn, $sql );
+	mysqli_close ( $conn );
 }
-
-function insertUserInfo($name, $age, $sex, $userinfo)
-{
-    $conn = creatCon();
-    $sql = "insert into userinfo (name,age,sex,userinfo) values(" . $name . "," . $age . "," . $sex . "," . $userinfo . "')";
-    mysqli_query($conn, $sql);
-    mysqli_close($conn);
+function insertUserInfo($name, $age, $sex, $userinfo) {
+	$conn = creatCon ();
+	$sql = "insert into userinfo (name,age,sex,userinfo) values('" . $name . "','" . $age . "','" . $sex . "','" . $userinfo . "')";
+	mysqli_query ( $conn, $sql );
+	mysqli_close ( $conn );
 }
