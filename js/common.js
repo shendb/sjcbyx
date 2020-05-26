6ff616af75a65076e800dafc9c01b61e6065ce00
DianBing.Shen <shendb0@gmail.com>
2020-05-26 22:41:28 +0800

    页面修正  图片未修正

--- js/common.js
+++ js/common.js
@@ -22,7 +22,6 @@ function imgList() {
         idStr = "";
         switch (i) {
             case 1:
-                imgStr += "<tr><td style='text-align: right;' colspan='2'><input type='submit' value='保存'></td></tr>";
                 idStr = "#imgStartBefore";
                 break;
             case 2:
@@ -34,10 +33,10 @@ function imgList() {
         }
         imgStr += "<tr style='border-top:1px solid'><td>手术前</td><td>手术后一周</td></tr>";
         imgStr += "<tr><td><img width='100%'  src='./asset/img/" + i + "/1.jpg' ></td><td><img width='100%'  src='./asset/img/" + i + "/2.jpg' ></td></tr>";
-        imgStr += "<tr><td style='text-align:left;' colspan='2'>" + i + "A:<input type='radio' name='imgWeek_" + i + "' value='1'>1<input type='radio' name='imgWeek_" + i + "' value='2'>2<input type='radio' name='imgWeek_" + i + "' value='3'>3<input type='radio' name='imgWeek_" + i + "' value='4'>4<input type='radio' name='imgWeek_" + i + "' value='5'>5<input type='radio' name='imgWeek_" + i + "' value='6'>6<input type='radio' name='imgWeek_" + i + "' value='7'>7<input type='radio' name='imgWeek_" + i + "' value='8'>8<input type='radio' name='imgWeek_" + i + "' value='9'>9<input type='radio' name='imgWeek_" + i + "' value='10'>10</td></tr>";
+        imgStr += "<tr><td style='text-align:left;' colspan='2'>" + i + "A:<input type='radio' name='imgWeek_" + i + "' value='0'>0<input type='radio' name='imgWeek_" + i + "' value='1'>1<input type='radio' name='imgWeek_" + i + "' value='2'>2<input type='radio' name='imgWeek_" + i + "' value='3'>3<input type='radio' name='imgWeek_" + i + "' value='4'>4<input type='radio' name='imgWeek_" + i + "' value='5'>5<input type='radio' name='imgWeek_" + i + "' value='6'>6<input type='radio' name='imgWeek_" + i + "' value='7'>7<input type='radio' name='imgWeek_" + i + "' value='8'>8<input type='radio' name='imgWeek_" + i + "' value='9'>9</td></tr>";
         imgStr += "<tr><td>手术前</td><td>手术后一个月</td></tr>";
         imgStr += "<tr><td><img width='100%'  src='./asset/img/" + i + "/1.jpg' ></td><td><img width='100%'  src='./asset/img/" + i + "/3.jpg' ></td></tr>";
-        imgStr += "<tr id='img" + i + "'><td style='text-align:left;' colspan='2'>" + i + "B:<input type='radio' name='imgMonth_" + i + "' value='1'>1<input type='radio' name='imgMonth_" + i + "' value='2'>2<input type='radio' name='imgMonth_" + i + "' value='3'>3<input type='radio' name='imgMonth_" + i + "' value='4'>4<input type='radio' name='imgMonth_" + i + "' value='5'>5<input type='radio' name='imgMonth_" + i + "' value='6'>6<input type='radio' name='imgMonth_" + i + "' value='7'>7<input type='radio' name='imgMonth_" + i + "' value='8'>8<input type='radio' name='imgMonth_" + i + "' value='9'>9<input type='radio' name='imgMonth_" + i + "' value='10'>10</td></tr>";
+        imgStr += "<tr id='img" + i + "'><td style='text-align:left;' colspan='2'>" + i + "B:<input type='radio' name='imgMonth_" + i + "' value='0'>0<input type='radio' name='imgMonth_" + i + "' value='1'>1<input type='radio' name='imgMonth_" + i + "' value='2'>2<input type='radio' name='imgMonth_" + i + "' value='3'>3<input type='radio' name='imgMonth_" + i + "' value='4'>4<input type='radio' name='imgMonth_" + i + "' value='5'>5<input type='radio' name='imgMonth_" + i + "' value='6'>6<input type='radio' name='imgMonth_" + i + "' value='7'>7<input type='radio' name='imgMonth_" + i + "' value='8'>8<input type='radio' name='imgMonth_" + i + "' value='9'>9</td></tr>";
         $(idStr).after(imgStr);
     }
 }
