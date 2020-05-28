2b788bd72bddb92a71060e5161cbd63976cb66b4
DianBing.Shen <shendb0@gmail.com>
2020-05-29 00:54:11 +0800

    图形按钮 追加 js变更，php变更

--- db.php
+++ db.php
@@ -1,38 +1,46 @@
 <?php
-error_reporting ( E_ALL ^ E_NOTICE );
-function creatCon() {
-	$conn = mysqli_connect ( "localhost", "root", "4rfv3edc", "sjcbyx", 3306 );
+error_reporting(E_ALL ^ E_NOTICE);
 
-	// 判断是否连接成功
-	if (! $conn) {
-		echo ("数据库连接失败！" . mysqli_connect_error ());
-	}
+function creatCon()
+{
+    $conn = mysqli_connect("localhost", "root", "4rfv3edc", "sjcbyx", 3306);
 
-	// 选择数据库
-	mysqli_select_db ( $conn, "sjcbyx" );
-	return $conn;
+    // 判断是否连接成功
+    if (! $conn) {
+        echo ("数据库连接失败！" . mysqli_connect_error());
+    }
+
+    // 选择数据库
+    mysqli_select_db($conn, "sjcbyx");
+    return $conn;
 }
-function selectUserInfo($name) {
-	$conn = creatCon ();
-	$sql = "select * from userinfo where name='" . $name . "'";
-	$result = mysqli_query ( $conn, $sql );
-	if (is_resource ( $result )) {
-		mysqli_close ( $conn );
-		return true;
-	} else {
-		mysqli_close ( $conn );
-		return false;
-	}
+
+function selectUserInfo($name)
+{
+    $conn = creatCon();
+    $sql = "select * from userinfo where name='" . $name . "'";
+    $result = mysqli_query($conn, $sql);
+    if (is_resource($result)) {
+        mysqli_close($conn);
+        return true;
+    } else {
+        mysqli_close($conn);
+        return false;
+    }
 }
-function updateUserInfo($name, $age, $sex, $userinfo) {
-	$conn = creatCon ();
-	$sql = "update userinfo set userinfo='" . $userinfo . "' where name='" . $name . "' and age='" . $age . "' and sex='" . $sex . "'";
-	mysqli_query ( $conn, $sql );
-	mysqli_close ( $conn );
+
+function updateUserInfo($name, $baseInfo, $table1, $table2, $table3, $table4, $hengB, $hengL, $shuB, $shuL, $fangB, $fangL, $yuanB, $yuanL)
+{
+    $conn = creatCon();
+    $sql = "update userinfo set baseInfo='" . $baseInfo . "'" . ", table1='" . $table1 . "'" . ", table2='" . $table2 . "'" . ", table3='" . $table3 . "'" . ", table4='" . $table4 . "'" . ", hengB='" . $hengB . "'" . ", hengL='" . $hengL . "'" . ", shuB='" . $shuB . "'" . ", shuL='" . $shuL . "'" . ", fangB='" . $fangB . "'" . ", fangL='" . $fangL . "'" . ", yuanB='" . $yuanB . "'" . ", yuanL='" . $yuanL . "'" . " where name='" . $name . "'";
+    mysqli_query($conn, $sql);
+    mysqli_close($conn);
 }
-function insertUserInfo($name, $age, $sex, $userinfo) {
-	$conn = creatCon ();
-	$sql = "insert into userinfo (name,age,sex,userinfo) values('" . $name . "','" . $age . "','" . $sex . "','" . $userinfo . "')";
-	mysqli_query ( $conn, $sql );
-	mysqli_close ( $conn );
+
+function insertUserInfo($name, $baseInfo, $table1, $table2, $table3, $table4, $hengB, $hengL, $shuB, $shuL, $fangB, $fangL, $yuanB, $yuanL)
+{
+    $conn = creatCon();
+    $sql = "insert into userinfo (name,baseInfo,table1,table2,table3,table4,hengB,hengL,shuB,shuL,fangB,fangL,yuanB,yuanL) values('" . $name . "','" . $baseInfo . "','" . $table1 . "','" . $table2 . "','" . $table3 . "','" . $table4 . "','" . $hengB . "','" . $hengL . "','" . $shuB . "','" . $shuL . "','" . $fangB . "','" . $fangL . "','" . $yuanB . "','" . $yuanL . "')";
+    mysqli_query($conn, $sql);
+    mysqli_close($conn);
 }
--- js/common.js
+++ js/common.js
@@ -1,12 +1,10 @@
 var time = 0;
 var Width = 0;
 var toggleFlg = false;
-var interval1 = '';
-var interval2 = '';
-var interval3 = '';
-var interval4 = '';
+
 window.onload = function() {
-    imgList();
+	imgList();
+	sessionStorage.clear();
 }
 
 function checkInput() {
@@ -14,114 +12,139 @@ function checkInput() {
 }
 
 function imgList() {
-    var imgStr = "";
-    var idStr = "";
+	var imgStr = "";
+	var idStr = "";
 
-    for (var i = 1; i <= 41; i++) {
-        imgStr = "";
-        idStr = "";
-        switch (i) {
-            case 1:
-                idStr = "#imgStartBefore";
-                break;
-            case 2:
-                idStr = "#img1";
-                break;
-            default:
-                idStr = "#img" + (i - 1);
-                break;
-        }
-        imgStr += "<tr style='border-top:1px solid'><td>手术前</td><td>手术后一周</td></tr>";
-        imgStr += "<tr><td><img width='100%'  src='./asset/img/" + i + "/1.jpg' ></td><td><img width='100%'  src='./asset/img/" + i + "/2.jpg' ></td></tr>";
-        imgStr += "<tr><td style='text-align:left;' colspan='2'>" + i + "A:<input type='radio' name='imgWeek_" + i + "' value='0'>0<input type='radio' name='imgWeek_" + i + "' value='1'>1<input type='radio' name='imgWeek_" + i + "' value='2'>2<input type='radio' name='imgWeek_" + i + "' value='3'>3<input type='radio' name='imgWeek_" + i + "' value='4'>4<input type='radio' name='imgWeek_" + i + "' value='5'>5<input type='radio' name='imgWeek_" + i + "' value='6'>6<input type='radio' name='imgWeek_" + i + "' value='7'>7<input type='radio' name='imgWeek_" + i + "' value='8'>8<input type='radio' name='imgWeek_" + i + "' value='9'>9</td></tr>";
-        imgStr += "<tr><td>手术前</td><td>手术后一个月</td></tr>";
-        imgStr += "<tr><td><img width='100%'  src='./asset/img/" + i + "/1.jpg' ></td><td><img width='100%'  src='./asset/img/" + i + "/3.jpg' ></td></tr>";
-        imgStr += "<tr id='img" + i + "'><td style='text-align:left;' colspan='2'>" + i + "B:<input type='radio' name='imgMonth_" + i + "' value='0'>0<input type='radio' name='imgMonth_" + i + "' value='1'>1<input type='radio' name='imgMonth_" + i + "' value='2'>2<input type='radio' name='imgMonth_" + i + "' value='3'>3<input type='radio' name='imgMonth_" + i + "' value='4'>4<input type='radio' name='imgMonth_" + i + "' value='5'>5<input type='radio' name='imgMonth_" + i + "' value='6'>6<input type='radio' name='imgMonth_" + i + "' value='7'>7<input type='radio' name='imgMonth_" + i + "' value='8'>8<input type='radio' name='imgMonth_" + i + "' value='9'>9</td></tr>";
-        $(idStr).after(imgStr);
-    }
+	for (var i = 1; i <= 41; i++) {
+		imgStr = "";
+		idStr = "";
+		switch (i) {
+		case 1:
+			idStr = "#imgStartBefore";
+			break;
+		case 2:
+			idStr = "#img1";
+			break;
+		default:
+			idStr = "#img" + (i - 1);
+			break;
+		}
+		imgStr += "<tr style='border-top:1px solid'><td>手术前</td><td>手术后一周</td></tr>";
+		imgStr += "<tr><td><img width='100%'  src='./asset/img/" + i
+				+ "/1.jpg' ></td><td><img width='100%'  src='./asset/img/" + i
+				+ "/2.jpg' ></td></tr>";
+		imgStr += "<tr><td style='text-align:left;' colspan='2'>" + i
+				+ "A:<input type='radio' name='imgWeek_" + i
+				+ "' value='0'>0<input type='radio' name='imgWeek_" + i
+				+ "' value='1'>1<input type='radio' name='imgWeek_" + i
+				+ "' value='2'>2<input type='radio' name='imgWeek_" + i
+				+ "' value='3'>3<input type='radio' name='imgWeek_" + i
+				+ "' value='4'>4<input type='radio' name='imgWeek_" + i
+				+ "' value='5'>5<input type='radio' name='imgWeek_" + i
+				+ "' value='6'>6<input type='radio' name='imgWeek_" + i
+				+ "' value='7'>7<input type='radio' name='imgWeek_" + i
+				+ "' value='8'>8<input type='radio' name='imgWeek_" + i
+				+ "' value='9'>9</td></tr>";
+		imgStr += "<tr><td>手术前</td><td>手术后一个月</td></tr>";
+		imgStr += "<tr><td><img width='100%'  src='./asset/img/" + i
+				+ "/1.jpg' ></td><td><img width='100%'  src='./asset/img/" + i
+				+ "/3.jpg' ></td></tr>";
+		imgStr += "<tr id='img" + i
+				+ "'><td style='text-align:left;' colspan='2'>" + i
+				+ "B:<input type='radio' name='imgMonth_" + i
+				+ "' value='0'>0<input type='radio' name='imgMonth_" + i
+				+ "' value='1'>1<input type='radio' name='imgMonth_" + i
+				+ "' value='2'>2<input type='radio' name='imgMonth_" + i
+				+ "' value='3'>3<input type='radio' name='imgMonth_" + i
+				+ "' value='4'>4<input type='radio' name='imgMonth_" + i
+				+ "' value='5'>5<input type='radio' name='imgMonth_" + i
+				+ "' value='6'>6<input type='radio' name='imgMonth_" + i
+				+ "' value='7'>7<input type='radio' name='imgMonth_" + i
+				+ "' value='8'>8<input type='radio' name='imgMonth_" + i
+				+ "' value='9'>9</td></tr>";
+		$(idStr).after(imgStr);
+	}
 }
 
 function Cal(square) {
-    switch (time) {
-        case 0:
-            time = time + 1;
-            toggleFlg = true;
-            break;
-        case 60:
-            time = time - 1;
-            toggleFlg = false;
-            break;
-        default:
-            if (toggleFlg) {
-                time = time + 1;
-            } else {
-                time = time - 1;
-            }
-            break;
-    }
-    Width = 60 + time;
-    switch (square) {
-        case 1:
-            document.getElementById("heng").style.width = Width + "px";
-            document.getElementById("hengV").value = Width;
-            break;
-        case 2:
-            document.getElementById("shu").style.height = Width + "px";
-            document.getElementById("shuV").value = Width;
-            break;
-        case 3:
-            document.getElementById("fang").style.width = Width + "px";
-            document.getElementById("fang").style.height = Width + "px";
-            document.getElementById("fangV").value = Width;
-            break;
-        case 4:
-            document.getElementById("yuan").style.width = Width + "px";
-            document.getElementById("yuan").style.height = Width + "px";
-            document.getElementById("yuanV").value = Width;
-            break;
-        default:
-            break;
-    }
+	if (sessionStorage.getItem("cal") != square) {
+		sessionStorage.setItem("cal", square);
+		time = 0;
+		toggleFlg = 1;
+	}
+
+	if(time>=15){
+		toggleFlg = 0;
+	}
+	if (time<=-15){
+		toggleFlg = 1;
+	}
+	switch (toggleFlg) {
+	case 1:
+		time = time + 1;
+		break;
+	case 0:
+		time = time - 1;
+		break;
+	default:
+		break;
+	}
+	Width = 60 + time;
+	switch (square) {
+	case 1:
+		document.getElementById("heng").style.width = Width + "px";
+		break;
+	case 2:
+		document.getElementById("shu").style.height = Width + "px";
+		break;
+	case 3:
+		document.getElementById("fang").style.width = Width + "px";
+		document.getElementById("fang").style.height = Width + "px";
+		break;
+	case 4:
+		document.getElementById("yuan").style.width = Width + "px";
+		document.getElementById("yuan").style.height = Width + "px";
+		break;
+	default:
+		break;
+	}
 }
 
-
-function squareClick(square) {
-    time = 0;
-    switch (square) {
-        case 1:
-            interval1 = setInterval("Cal(1)", 1000);
-
-            break;
-        case 2:
-            interval2 = setInterval("Cal(2)", 1000);
-            break;
-        case 3:
-            interval3 = setInterval("Cal(3)", 1000);
-            break;
-        case 4:
-            interval4 = setInterval("Cal(4)", 1000);
-            break;
-        default:
-            break;
-    }
-
+function change2big(square){
+	var Width = 60 + time;
+	switch (square) {
+	case 1:
+		document.getElementById("hengB").value = Width;
+		break;
+	case 2:
+		document.getElementById("shuB").value = Width;
+		break;
+	case 3:
+		document.getElementById("fangB").value = Width;
+		break;
+	case 4:
+		document.getElementById("yuanB").value = Width;
+		break;
+	default:
+		break;
+	}
 }
-
-function squareCancle(square) {
-    switch (square) {
-        case 1:
-            clearInterval(interval1);
-            break;
-        case 2:
-            clearInterval(interval2);
-            break;
-        case 3:
-            clearInterval(interval3);
-            break;
-        case 4:
-            clearInterval(interval4);
-            break;
-        default:
-            break;
-    }
+function change2little(square){
+	var Width = 60 + time;
+	switch (square) {
+	case 1:
+		document.getElementById("hengL").value = Width;
+		break;
+	case 2:
+		document.getElementById("shuL").value = Width;
+		break;
+	case 3:
+		document.getElementById("fangL").value = Width;
+		break;
+	case 4:
+		document.getElementById("yuanL").value = Width;
+		break;
+	default:
+		break;
+	}
 }
